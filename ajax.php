0b1a8bf3d3bc8d8263f47b4f3556c600bd357f13
Julio Foulquié <jfoulquie@gmail.com>
2015-03-07 21:53:43 +0100

    New version of ajax controller, more flexible now.

--- ajax.php
+++ ajax.php
@@ -2,21 +2,31 @@
 
 require('config.php');
 require("functions.php");
+
+
 if(!isset($_REQUEST['username'])){
 	echo json_encode(array('result' => 401, 'error' => 'Username missing.'));
 	exit;
 }
 
-if(!is_valid_username($_REQUEST['username'])) {
+if(!function_exists($_REQUEST['network'])) {
+	echo json_encode(array('result' => 401, 'error' => 'That is not a valid network.'));
+	exit;
+} else {
+	$network = filter_input(INPUT_POST, 'network', FILTER_SANITIZE_STRING);
+}
+$req = trim($_REQUEST['username']);
+if(!is_valid_username($req)) {
 	echo json_encode(array('result' => 401, 'error' => 'That is not a valid username.'));
 	exit;
+} else {
+	$username = trim(filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING));
 }
-
 if(isset($_REQUEST['max_pics']) && intval($_REQUEST['max_pics'])) {
 	$max_number_images = intval($_REQUEST['max_pics']);
 }
 
 
-$result = instagram($_REQUEST['username']);
+$result = $network($username);
 
 echo json_encode($result);
