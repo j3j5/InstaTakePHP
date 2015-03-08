c4f223098abf357cdc7ffa4f2bd1335e127311f3
Julio Foulquié <jfoulquie@gmail.com>
2015-03-08 14:10:38 +0100

    Start using output_buffer on the index.php.

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
+
+ob_start();
+
 include('config.php');
 
 include('routing.php');
 
 require('functions.php');
 
+// Load the controller
 include(__DIR__ . DIRECTORY_SEPARATOR ."controllers" . DIRECTORY_SEPARATOR ."$controller.php");
 
+// Don't try to load a non existant view
 if(!is_file(__DIR__ . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "$view.php")) {
 	$view = '404';
 }
@@ -16,3 +21,5 @@ include(__DIR__ . DIRECTORY_SEPARATOR ."views" . DIRECTORY_SEPARATOR ."header.ph
 include(__DIR__ . DIRECTORY_SEPARATOR ."views" . DIRECTORY_SEPARATOR ."$view.php");
 
 include(__DIR__ . DIRECTORY_SEPARATOR ."views" . DIRECTORY_SEPARATOR ."footer.php");
+
+ob_end_flush();
